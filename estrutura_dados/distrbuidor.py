6a4f89e07eb26db92aa09ebcb48b6fb04e136ac0
rubes <rubensrissi@hotmail.com>
2020-06-07 21:44:01 -0300

    wip.

--- estrutura_dados/distrbuidor.py
+++ estrutura_dados/distrbuidor.py
@@ -1,15 +1,17 @@
+from datetime import date
+
+
 class Distribuidor(object):
     def __init__(self, *args) -> None:
         """id , nome, cnpj, contato, nivel, nome_pai, pecas_vendidas, last_up(date)"""
 
-        self._dist_id: int = args[0]
-        self._dist_nome: str = args[1]
-        self._cnpj: int = args[2]
-        self._contato: int = args[3]
-        self._nivel: str = args[4]
-        self._nome_pai: str = args[5]
-        self._pecas_vendidas: float = args[6]
-        self._last_update: str
+        self._dist_nome: str = args[0]
+        self._cnpj: int = args[1]
+        self._contato: int = args[2]
+        self._nivel: str = args[3]
+        self._nome_pai: str = args[4]
+        self._pecas_vendidas: float = args[5]
+        self._last_update: date = date.today()
         self._n_dist_formado: int
 
     def get_dist_id(self) -> int:
@@ -36,7 +38,7 @@ class Distribuidor(object):
     def get_n_dist_formado(self) -> int:
         return self._n_dist_formado
 
-    def get_last_update(self) -> str:
+    def get_last_update(self) -> date:
         return self._last_update
 
     def set_nivel(self, nivel) -> str:
